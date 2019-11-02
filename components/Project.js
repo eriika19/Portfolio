b8c4cb31bb08aa6f9cdc7daff25dc0abd3b5dde9
eriika19 <enciso.iq@gmail.com>
2019-11-01 19:25:11 -0600

    se hará primer push. No se pudo crear carpetas para modo de producción

--- components/Project.js
+++ components/Project.js
@@ -13,10 +13,10 @@ class Project extends Component {
     return (
       <Link as={`/project/${id}`} href={`/project?id=${id}`}>
         <div className="card" id={`project-${id}`} onMouseEnter={this.prefetchProjectPage}>
-        <img className="" src={src} alt={`Image for ${title}`} width="300"/>                  
+        <img src={src} alt={`Image for ${title}`} width="300"/>                  
           {/* <div className="card-image">
             <Image src={img[thumbnail]} alt={`Image for ${title}`} className="img-responsive" />
-          </div> */}
+          </div> */}git 
           <div className="card-header">
             <div className="card-title h5 text-primary text-ellipsis">{title}</div>
           </div>
